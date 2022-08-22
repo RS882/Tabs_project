9b87ef62750adcff00f76a2b6c6593dec4b3c293
Russen <russen333@gmail.com>
2022-08-22 11:25:32 +0300

    -rename dir

--- #src/js/moduls/tabs.js
+++ #src/js/moduls/tabs.js
@@ -1,25 +1,40 @@
 const tabs = (tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) => {
 	//tabs=======================================
+
+	// класс для scss
+	//	.fade {
+	// 		animation-name: fade;
+	// 		animation-duration: 1.5s;
+	// 		@keyframes fade {
+	// 			 from {
+	// 				  opacity: 0.1;
+	// 			 }
+	// 			 to {
+	// 				  opacity: 1;
+	// 			 }
+	// 		}
+	//   }
 	const tabs = document.querySelectorAll(tabsSelector),
 		tabsContent = document.querySelectorAll(tabsContentSelector),
 		tabsParent = document.querySelector(tabsParentSelector);
 
 	const hideTabContent = () => {
 		tabsContent.forEach(item => {
-			item.classList.add(`hide`);
-			item.classList.remove(`show`, `fade`);
+			item.hidden = true;
+			item.classList.remove(`fade`);
+
 		});
 		tabs.forEach(item => item.classList.remove(activeClass));
-	}
+	};
 	const showTabContent = (i = 0) => {
-		tabsContent[i].classList.add(`show`, `fade`);
-		tabsContent[i].classList.remove(`hide`);
+		tabsContent[i].hidden = false;
+		tabsContent[i].classList.add(`fade`);
 		tabs[i].classList.add(activeClass);
-	}
+	};
 	hideTabContent();
 	showTabContent();
-	tabsParent.addEventListener(`click`, (event) => {
-		const target = event.target;
+	tabsParent.addEventListener(`click`, (e) => {
+		const target = e.target;
 		if (target && target.classList.contains(tabsSelector.slice(1))) {
 			tabs.forEach((item, i) => {
 				if (target == item) {
